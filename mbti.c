2a5e6ea7c373b0549e822ce21469003e56858f98
codms0201 <chanyeol0615@naver.com>
2023-05-18 18:59:16 +0900

    update mbti.c. Add struct Mbti, createMbti().

--- mbti.c
+++ mbti.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #define MAX_PEOPLE 100
 
+typedef struct{
+        char mbti[MAX_PEOPLE]; //자신의 mbti
+        char name[100]; //이름
+        char department[12]; //학부
+        int s_num; //학번
+        char sex; //성별
+} Mbti;
+
 int selectMenu() {
     int menu;
     printf("\n*** mbti 결과 분석 서비스 ***\n");
@@ -20,3 +28,17 @@ int selectMenu() {
     return menu;
 }
 
+int createMbti(Mbti *m){
+        printf("이름은? ");
+        scanf("%s", m->name);
+        printf("성별은? ");
+        scanf("%s", m->sex);
+        printf("학부는? ");
+        scanf("%s", m->department);
+        printf("학번은? ");
+        scanf("%d", &m->s_num);
+        printf("MBTI는? ");
+        scanf("%s", m->mbti);
+        printf("=> 추가됨!\n");
+        return 1;
+}
