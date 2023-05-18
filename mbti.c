abff0334b691243f486e85d7bf9473c4fddc3936
codms0201 <kellykwon58@handong.ac.kr>
2023-05-18 16:28:37 +0900

    add mbti.c and make selectMenu function

---
+++ mbti.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int selectMenu() {
+    int menu;
+    printf("\n*** mbti 결과 분석 서비스 ***\n");
+    printf("1. 조회\n");
+    printf("2. 추가\n");
+    printf("3. 수정\n");
+    printf("4. 삭제\n");
+    printf("5. 저장\n");
+    printf("6. 검색\n");
+    printf("7. 분석\n");
+    printf("8. mbti에 관한 정보\n");
+    printf("0. 종료\n\n");
+    printf("=> 원하는 메뉴는? ");
+    scanf("%d", &menu);
+    return menu;
+}
