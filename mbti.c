766e1f99ee9a64eae505dae7ba94819df867dfcf
codms0201 <chanyeol0615@naver.com>
2023-05-18 18:56:32 +0900

    Make mbti.c(I re-do this because my grass did not updated by different email)

--- mbti.c
+++ mbti.c
@@ -3,14 +3,6 @@
 #include <string.h>
 #define MAX_PEOPLE 100
 
-typedef struct{
-	char mbti[MAX_PEOPLE]; //자신의 mbti
-	char name[100]; //이름
-	char department[12]; //학부
-	int s_num; //학번
-	char sex; //성별
-} Mbti;
-
 int selectMenu() {
     int menu;
     printf("\n*** mbti 결과 분석 서비스 ***\n");
@@ -28,46 +20,3 @@ int selectMenu() {
     return menu;
 }
 
-int createMbti(Mbti *m){
-	printf("이름은? ");
-	scanf("%s", m->name);
-	printf("성별은? ");
-	scanf("%s", m->sex);
-	printf("학부는? ");
-	scanf("%s", m->department);
-	printf("학번은? ");
-	scanf("%d", &m->s_num);
-	printf("MBTI는? ");
-	scanf("%s", m->mbti);
-	printf("=> 추가됨!\n");
-	return 1;
-}
-
-void readMbit(Mbti m){
-	printf("%s %d %s %s %s\n", m->name, m->s_num, m->sex, m->department, m->mbti);
-}
-
-void listMbti(Mbti *m[], int count){
-    printf("\nNo Name Student_number Sex Department Mbti\n");
-    printf("================================================\n");
-    for(int i = 0; i < count; i++){
-        printf("%-2d", i+1);
-        readMbti(*m[i]);
-    }
-    printf("\n");
-}
-
-int updateMbti(Mbti *m){
-        printf("이름은? ");
-        scanf("%s", m->name);
-        printf("성별은? ");
-        scanf("%s", m->sex);
-        printf("학부는? ");
-        scanf("%s", m->department);
-        printf("학번은? ");
-        scanf("%d", &m->s_num);
-        printf("MBTI는? ");
-        scanf("%s", m->mbti);
-        printf("=> 수정됨!\n");
-        return 1;
-}
